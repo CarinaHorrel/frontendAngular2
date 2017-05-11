f072928c1cd0631ef5efb21fd1fdab2e68ea04d2
Carina Horrel <Carina Horrel>
2017-05-11 13:07:45 +0200

    adding components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,24 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ActorComponent } from './actor/actor.component';
+import { ArtistComponent } from './artist/artist.component';
+import { WriterComponent } from './writer/writer.component';
+import { DvdComponent } from './dvd/dvd.component';
+import { CdComponent } from './cd/cd.component';
+import { BookComponent } from './book/book.component';
+import { GenreComponent } from './genre/genre.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ActorComponent,
+    ArtistComponent,
+    WriterComponent,
+    DvdComponent,
+    CdComponent,
+    BookComponent,
+    GenreComponent
   ],
   imports: [
     BrowserModule,
